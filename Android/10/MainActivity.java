935e79d3f7cacde05b4d41a22c8baadeed651947
theresathankachan <88645655+theresathankachan@users.noreply.github.com>
2022-10-11 15:40:26 +0530

    Update MainActivity.java

--- Android/10/MainActivity.java
+++ Android/10/MainActivity.java
@@ -1,64 +1,39 @@
-package com.example.birthdaycard;
+<?xml version="1.0" encoding="utf-8"?>
+<FrameLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:orientation="vertical"
+    tools:context=".MainActivity">
 
-import android.os.Bundle;
+    <ImageView
+        android:id="@+id/img1"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:src="@drawable/im1" />
+    <ImageView
+        android:id="@+id/img2"
+        android:layout_width="match_parent"
+        android:layout_height="232dp"
+        app:srcCompat="@drawable/im2"
+        android:visibility="gone"/>
 
-import com.google.android.material.snackbar.Snackbar;
+    <ImageView
+        android:id="@+id/img3"
+        android:layout_width="match_parent"
+        android:layout_height="275dp"
+        android:layout_gravity="center"
+        app:srcCompat="@drawable/im4"
+        android:visibility="gone"/>
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.birthdaycard.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ImageView;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
-    ImageView i1, i2,i3,i4,i5;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        i1 = (ImageView) findViewById(R.id.img1);
-        i2 = (ImageView) findViewById(R.id.img2);
-        i3 = (ImageView) findViewById(R.id.img3);
-        i4 = (ImageView) findViewById(R.id.img4);
-        i5 = (ImageView) findViewById(R.id.img5);
-        i1.setOnClickListener(this);
-        i2.setOnClickListener(this);
-        i3.setOnClickListener(this);
-        i4.setOnClickListener(this);
-        i5.setOnClickListener(this);
-    }
+    <ImageView
+        android:id="@+id/img4"
+        android:layout_width="match_parent"
+        android:layout_height="270dp"
+        android:layout_gravity="bottom"
+        app:srcCompat="@drawable/im5"
+        android:visibility="gone"/>
 
 
-    @Override
-    public void onClick(View view) {
-        if (view.getId() == R.id.img1) {
-            i1.setVisibility(View.GONE);
-            i2.setVisibility(View.VISIBLE);
-
-
-        }
-        if (view.getId() == R.id.img2) {
-            i2.setVisibility(View.GONE);
-            i3.setVisibility(View.VISIBLE);
-        }
-        if (view.getId() == R.id.img3) {
-            i3.setVisibility(View.GONE);
-            i4.setVisibility(View.VISIBLE);
-        }
-        if (view.getId() == R.id.img4) {
-            i4.setVisibility(View.GONE);
-            i1.setVisibility(View.VISIBLE);
-        }
-    }
-}
+</FrameLayout>
